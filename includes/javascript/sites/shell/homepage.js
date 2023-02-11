52fc29e662b95b2fc5739a2f2ec0a58ef1b9c16e
thetoonsofjosh <111701431+thetoonsofjosh@users.noreply.github.com>
2023-02-11 12:54:07 -0500

    Add files via upload

---
+++ includes/javascript/sites/shell/homepage.js
@@ -0,0 +1,13 @@
+/*****************************************************************************
+
+Homepage site include.
+
+
+*****************************************************************************/
+
+var config = {
+    'homepage': true
+}
+
+// Include the master JS include.
+document.write('<script type="text/javascript" src="/includes/javascript/sites/v2.2/pbskids.js" ></script>');
