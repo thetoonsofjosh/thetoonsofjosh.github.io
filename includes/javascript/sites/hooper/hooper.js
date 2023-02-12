1a6ed34f2ce4500296f8f8c7f01bb51793f5b4cf
thetoonsofjosh <111701431+thetoonsofjosh@users.noreply.github.com>
2023-02-11 19:14:49 -0500

    Add files via upload

---
+++ includes/javascript/sites/hooper/hooper.js
@@ -0,0 +1,14 @@
+/*****************************************************************************
+
+Hooper site include.
+
+
+*****************************************************************************/
+
+var config = {
+    'sponsorship': true,
+    'bridge': true
+}
+
+// Include the master JS include.
+document.write('<script type="text/javascript" src="/includes/javascript/sites/v2.0/pbskids.js" ></script>');
