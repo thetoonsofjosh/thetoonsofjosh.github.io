d258e059ec9870409104dd6fe4552eb8f74d050d
thetoonsofjosh <111701431+thetoonsofjosh@users.noreply.github.com>
2023-04-04 13:07:06 -0400

    Add files via upload

---
+++ includes/javascript/sites/shell/go.js
@@ -0,0 +1,16 @@
+/*****************************************************************************
+
+GO Homepage site include.
+
+
+*****************************************************************************/
+
+var config = {
+    'sponsorship': true,
+    'bridge+login': true, 
+    'go' : true	
+ 	
+}
+
+// Include the master JS include.
+document.write('<script type="text/javascript" src="/includes/javascript/sites/v2.2/pbskids.js" ></script>');
