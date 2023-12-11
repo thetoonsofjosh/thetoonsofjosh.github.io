def76b4baeb30152259864a63d0caca99afa1028
TheToonsOfJosh <111701431+thetoonsofjosh@users.noreply.github.com>
2023-12-11 17:53:20 -0500

    Add files via upload

---
+++ headband/config.js
@@ -0,0 +1,10 @@
+{
+            "reload": false,
+	    "brand" : "preschool",
+	    "parents_url" : "http://pbs.org/parents",
+	    "teachers_url" : "http://pbslearningmedia.org",    
+            "shop_partner": false,
+	    "shop_url" : "http://shop.pbs.org/",
+            "buttons_color": "#99CC00",
+            "go_login": true
+}
