881f3f39eba982acddc65e0601793766201c465c
TheToonsOfJosh <111701431+thetoonsofjosh@users.noreply.github.com>
2023-12-14 19:43:08 -0500

    Add files via upload

---
+++ danceparty/headband/config.js
@@ -0,0 +1,1 @@
+{"buttons_color": "", "shop_url": "http://shop.pbskids.org/?utm_source=brandsite&utm_medium=portalnav&utm_campaign=pbskidsnav", "go_login": true, "brand": "preschool", "destination": "", "reload": true, "parents_url": "http://pbs.org/parents", "shop_partner": true, "learning_goals": "", "teachers_url": "http://pbs.org/teachers"}
