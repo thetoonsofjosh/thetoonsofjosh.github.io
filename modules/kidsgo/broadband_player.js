2a464c74aa0817577299416fbe423273148d9819
TheToonsOfJosh <111701431+thetoonsofjosh@users.noreply.github.com>
2024-03-30 06:08:34 -0400

    Update broadband_player.js

--- modules/kidsgo/broadband_player.js
+++ modules/kidsgo/broadband_player.js
@@ -40,7 +40,7 @@ pbs.kidsgo.broadbandplayer.init = function(_stationID, _supersite, _configObj) {
     var communityAccountID = _configObj && _configObj.communityID ? _configObj.communityID : 824012198; // production community if no other community is set
     var host = _configObj && _configObj.host ? _configObj.host : "pbskids.org"; // production host if no other host is set
     var path = _configObj && _configObj.path ? _configObj.path : "/go/video/"; //production path if no other path is set
-    var cdn = _configObj && _configObj.cdn ? _configObj.cdn : "http://www-tc.pbskids.org/go/video/"; // production cdn if no other cdn is set
+    var cdn = _configObj && _configObj.cdn ? _configObj.cdn : "https://www-tc.pbskids.org/go/video/"; // production cdn if no other cdn is set
     var releasesEndIndex = 100;
     var categoriesEndIndex = 100;
     var showsEndIndex = 30;
@@ -62,9 +62,9 @@ pbs.kidsgo.broadbandplayer.init = function(_stationID, _supersite, _configObj) {
             _flashcontentDiv.style.width = '100%';
             _flashcontentDiv.style.height = '800px';
         }
-        var stationLogo = "http://www.pbs.org/images/stations/standard/"+_stationID+".gif";
+        var stationLogo = "https://www.pbs.org/images/stations/standard/"+_stationID+".gif";
 
-        var html_str ='<script type="text/javascript">var fo = new SWFObject("https://' + host + path + 'broadband_player.swf", "PBS Kids Go!", "100%", "100%", "9", "#ffffff");fo.addParam("wmode", "transparent");fo.addParam("quality", "high");fo.addParam("align", "middle");fo.addParam("play", "true");fo.addParam("loop", "true");fo.addParam("scale", "noScale");fo.addParam("wmode", "transparent");fo.addParam("devicefont", "false");fo.addParam("menu", "false");fo.addParam("allowFullScreen", "true");fo.addParam("allowNetworking", "all");fo.addParam("allowScriptAccess", "always");fo.addVariable("supersite", "'+_supersite+'");fo.addVariable("airdatesURL", "'+cdn+'airdates.swf");fo.addVariable("STATION_TVDATA_NAME", "'+_stationID+'");fo.addVariable("pidToPlayer", "'+urlPID+'");fo.addVariable("categoryToPlayer", "'+urlCategory+'");fo.addVariable("accountPortalID", "'+accountPortalID+'");fo.addVariable("stationLogo", "'+stationLogo+'");fo.addVariable("communityAccountID", '+communityAccountID+');fo.addVariable("releasesEndIndex", '+releasesEndIndex+');fo.addVariable("categoriesEndIndex", '+categoriesEndIndex+');fo.addVariable("showsEndIndex", '+showsEndIndex+');fo.addVariable("swfHost","'+cdn+'");fo.addVariable("newHeaderURL","http://www-tc.pbs.org/upload/theplatform/staging/http/images/kidsgo/headers/newVideos.jpg");'
+        var html_str ='<script type="text/javascript">var fo = new SWFObject("https://' + host + path + 'broadband_player.swf", "PBS Kids Go!", "100%", "100%", "9", "#ffffff");fo.addParam("wmode", "transparent");fo.addParam("quality", "high");fo.addParam("align", "middle");fo.addParam("play", "true");fo.addParam("loop", "true");fo.addParam("scale", "noScale");fo.addParam("wmode", "transparent");fo.addParam("devicefont", "false");fo.addParam("menu", "false");fo.addParam("allowFullScreen", "true");fo.addParam("allowNetworking", "all");fo.addParam("allowScriptAccess", "always");fo.addVariable("supersite", "'+_supersite+'");fo.addVariable("airdatesURL", "'+cdn+'airdates.swf");fo.addVariable("STATION_TVDATA_NAME", "'+_stationID+'");fo.addVariable("pidToPlayer", "'+urlPID+'");fo.addVariable("categoryToPlayer", "'+urlCategory+'");fo.addVariable("accountPortalID", "'+accountPortalID+'");fo.addVariable("stationLogo", "'+stationLogo+'");fo.addVariable("communityAccountID", '+communityAccountID+');fo.addVariable("releasesEndIndex", '+releasesEndIndex+');fo.addVariable("categoriesEndIndex", '+categoriesEndIndex+');fo.addVariable("showsEndIndex", '+showsEndIndex+');fo.addVariable("swfHost","'+cdn+'");fo.addVariable("newHeaderURL","https://www-tc.pbs.org/upload/theplatform/staging/https/images/kidsgo/headers/newVideos.jpg");'
 
     
            html_str += cdn == './' ? '' : 'PBS_enable_cdn(fo);';
@@ -72,7 +72,7 @@ pbs.kidsgo.broadbandplayer.init = function(_stationID, _supersite, _configObj) {
     
     } else {
     
-    html_str = '<style type="text/css">body {margin: 0px;height: 100%;width: 100%;}</style> <table border="0" cellspacing="0" cellpadding="0" width="100%" height="90%" valign="middle"><tr><td><div style="margin:auto; width:760px; height:376px"><img src="https://web.archive.org/web/20100329201816/http://pbskids.org/go/video/images/down.jpg" border="0" alt="Our site is down, but we will be back soon. Just play outside or clean your room!"></div></td></tr></table>' ; 
+    html_str = '<style type="text/css">body {margin: 0px;height: 100%;width: 100%;}</style> <table border="0" cellspacing="0" cellpadding="0" width="100%" height="90%" valign="middle"><tr><td><div style="margin:auto; width:760px; height:376px"><img src="https://web.archive.org/web/20100329201816/https://pbskids.org/go/video/images/down.jpg" border="0" alt="Our site is down, but we will be back soon. Just play outside or clean your room!"></div></td></tr></table>' ; 
     
     }
     
