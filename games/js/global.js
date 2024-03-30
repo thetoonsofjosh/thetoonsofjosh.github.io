04b53954e3d069ad0bb355dffaae2dcee4ae22bc
TheToonsOfJosh <111701431+thetoonsofjosh@users.noreply.github.com>
2024-03-30 13:45:04 -0400

    Add files via upload

--- games/js/global.js
+++ games/js/global.js
@@ -1,14 +1,7 @@
-var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
-if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
-{
-  let window = _____WB$wombat$assign$function_____("window");
-  let self = _____WB$wombat$assign$function_____("self");
-  let document = _____WB$wombat$assign$function_____("document");
-  let location = _____WB$wombat$assign$function_____("location");
-  let top = _____WB$wombat$assign$function_____("top");
-  let parent = _____WB$wombat$assign$function_____("parent");
-  let frames = _____WB$wombat$assign$function_____("frames");
-  let opener = _____WB$wombat$assign$function_____("opener");
+(function(b){var o=!1,d=null,u=parseFloat,j=String.fromCharCode,q=Math.min,l=/(-?\d+\.?\d*)$/g,g,a=[],h,m,t=9472,f={},c;if(!Array.indexOf){Array.prototype.indexOf=function(w){for(var v=0,s=this.length;v<s;v++){if(this[v]==w){return v}}return -1}}for(var p=32,k=j(p),r=255;p<r;p++,k=j(p).toLowerCase()){if(a.indexOf(k)!==-1){a.push(k)}}a.sort();b.tinysort={id:"TinySort",version:"1.3.27",copyright:"Copyright (c) 2008-2012 Ron Valstar",uri:"http://tinysort.sjeiti.com/",licenced:{MIT:"http://www.opensource.org/licenses/mit-license.php",GPL:"http://www.gnu.org/licenses/gpl.html"},defaults:{order:"asc",attr:d,data:d,useVal:o,place:"start",returns:o,cases:o,forceStrings:o,sortFunction:d,charOrder:g}};b.fn.extend({tinysort:function(V,L){if(V&&typeof(V)!="string"){L=V;V=d}var T=b.extend({},b.tinysort.defaults,L),v,Q=this,z=b(this).length,ae={},W=!(!V||V==""),H=!(T.attr===d||T.attr==""),ah=T.data!==d,J=W&&V[0]==":",C=J?Q.filter(V):Q,F=T.sortFunction,s=T.order=="asc"?1:-1,P=[];if(T.charOrder!=g){g=T.charOrder;if(!T.charOrder){m=false;t=9472;f={};c=h=d}else{h=a.slice(0);m=false;for(var S=[],B=function(i,ai){S.push(ai);f[T.cases?i:i.toLowerCase()]=ai},N="",X="z",aa=g.length,ac,Z,ad=0;ad<aa;ad++){var x=g[ad],ab=x.charCodeAt(),I=ab>96&&ab<123;if(!I){if(x=="["){var D=S.length,M=D?S[D-1]:X,w=g.substr(ad+1).match(/[^\]]*/)[0],R=w.match(/{[^}]*}/g);if(R){for(ac=0,Z=R.length;ac<Z;ac++){var O=R[ac];ad+=O.length;w=w.replace(O,"");B(O.replace(/[{}]/g,""),M);m=true}}for(ac=0,Z=w.length;ac<Z;ac++){B(M,w[ac])}ad+=w.length+1}else{if(x=="{"){var G=g.substr(ad+1).match(/[^}]*/)[0];B(G,j(t++));ad+=G.length+1;m=true}else{S.push(x)}}}if(S.length&&(I||ad===aa-1)){var E=S.join("");N+=E;b.each(E,function(i,ai){h.splice(h.indexOf(ai),1)});var A=S.slice(0);A.splice(0,0,h.indexOf(X)+1,0);Array.prototype.splice.apply(h,A);S.length=0}if(ad+1===aa){c=new RegExp("["+N+"]","gi")}else{if(I){X=x}}}}}if(!F){F=T.order=="rand"?function(){return Math.random()<0.5?1:-1}:function(av,at){var au=o,am=!T.cases?n(av.s):av.s,ak=!T.cases?n(at.s):at.s;if(!T.forceStrings){var aj=am&&am.match(l),aw=ak&&ak.match(l);if(aj&&aw){var ar=am.substr(0,am.length-aj[0].length),aq=ak.substr(0,ak.length-aw[0].length);if(ar==aq){au=!o;am=u(aj[0]);ak=u(aw[0])}}}var ai=s*(am<ak?-1:(am>ak?1:0));if(!au&&T.charOrder){if(m){for(var ax in f){var al=f[ax];am=am.replace(ax,al);ak=ak.replace(ax,al)}}if(am.match(c)!==d||ak.match(c)!==d){for(var ap=0,ao=q(am.length,ak.length);ap<ao;ap++){var an=h.indexOf(am[ap]),i=h.indexOf(ak[ap]);if(ai=s*(an<i?-1:(an>i?1:0))){break}}}}return ai}}Q.each(function(ak,al){var am=b(al),ai=W?(J?C.filter(al):am.find(V)):am,an=ah?""+ai.data(T.data):(H?ai.attr(T.attr):(T.useVal?ai.val():ai.text())),aj=am.parent();if(!ae[aj]){ae[aj]={s:[],n:[]}}if(ai.length>0){ae[aj].s.push({s:an,e:am,n:ak})}else{ae[aj].n.push({e:am,n:ak})}});for(v in ae){ae[v].s.sort(F)}for(v in ae){var ag=ae[v],K=[],Y=z,af=[0,0],ad;switch(T.place){case"first":b.each(ag.s,function(ai,aj){Y=q(Y,aj.n)});break;case"org":b.each(ag.s,function(ai,aj){K.push(aj.n)});break;case"end":Y=ag.n.length;break;default:Y=0}for(ad=0;ad<z;ad++){var y=e(K,ad)?!o:ad>=Y&&ad<Y+ag.s.length,U=(y?ag.s:ag.n)[af[y?0:1]].e;U.parent().append(U);if(y||!T.returns){P.push(U.get(0))}af[y?0:1]++}}Q.length=0;Array.prototype.push.apply(Q,P);return Q}});function n(i){return i&&i.toLowerCase?i.toLowerCase():i}function e(v,x){for(var w=0,s=v.length;w<s;w++){if(v[w]==x){return !o}}return o}b.fn.TinySort=b.fn.Tinysort=b.fn.tsort=b.fn.tinysort})(jQuery);
+
+
+
 
 jQuery(document).ready(function($) {
 
@@ -125,7 +118,7 @@ jQuery(document).ready(function($) {
 
 
 	// Billboard
-	if($(".billboard").length != 0) {
+	if($(".billboard").length != 0) { 
 
 		// Game Info
 		if($(".gameInfo").length != 0) {
@@ -180,7 +173,15 @@ jQuery(document).ready(function($) {
 				});
 			});
 		}
-
+		// Two:
+		if($(".grid-2").length != 0) {
+			$(".grid-2").each(function(){
+				$(this).find("li").each(function(){
+					myPos = $(this).index() + 1;
+					$(this).addClass("two-" + myPos);
+				});
+			});
+		}
 
 		// Functionality
 		$(".billboard aside a").mousedown(function(){
@@ -228,22 +229,30 @@ jQuery(document).ready(function($) {
 	} // end billboard existence check
 
 
-
-
-
-
 	// Slot Machine
+	$(".slotmachine .slots").css({ "height" : "auto", "bottom": "-9px", "top": "auto"}); //should move into page CSS. 
+	
+	// Reverses the DOM elements - used in slotmachine
+    (function($) {
+        $.fn.reverseOrder = function() {
+	      return this.each(function() {
+		    $(this).prependTo( $(this).parent() );
+	      });
+        };
+    })(jQuery);
+	
 	if($(".slotmachine").length != 0) {
-
+	
 		// Prepeare the lever
 		$(".slotmachine").append("<h3 id=\"lever\" />");
 		$("#lever").append("<em>Spotlighted </em>").append("<span>Games</span>");
 
 
 		// Re-sort the ULs (1-2-3 becomes 3-2-1)
-		$(".slots ul:eq(1)").prependTo(".slots");
-		$(".slots ul:eq(2)").prependTo(".slots");
+		$('.slots ul').reverseOrder();
 		
+		// Currently visable row gets class "current"
+		$(".slots ul:last").addClass("current");
 
 
 		// Tweak the markup
@@ -282,13 +291,20 @@ jQuery(document).ready(function($) {
 					});
 					// Whole animation complete.
 				});
+				
 				$(".slots").animate({
-					top: '+=59'
+					bottom: '-=59'
 				}, 630, function() { // Callback (re-orient everything).
-					$(this).css("top","-116px");
-					$(".slots ul:last").prependTo(".slots");
+					$(".slots ul.current").removeClass("current").addClass("former"); 
+					
+					$(".slots ul.former").prev().addClass("current");
+					$(".slots ul.former").clone().removeClass("former").prependTo(".slots");
+					$(".slots ul.former").remove();
+					
 					$("#invisiSlots ul").remove(); // Dump the old links.
-					$(".slots ul:last").clone().appendTo("#invisiSlots"); // Replace 'em with the new links.
+					$(".slots ul.current").clone().appendTo("#invisiSlots"); // Replace 'em with the new links.
+					
+					$(this).css("bottom","-9px");
 					// Whole animation complete.
 				});
 
@@ -398,29 +414,20 @@ jQuery(document).ready(function($) {
 
 		});
 	}
+	
+	// Adding sticky header scroll detection
+	$(document).bind('scroll', function() {
+	  var pagePosition = $(document).scrollTop();
+	  var headbandHeight = $('#headband-container').height();
+	
+	  if (pagePosition > headbandHeight){
+	    $('header.ledge').addClass('sticky');
+	  }
+	  if (pagePosition < headbandHeight){
+	     $('header.ledge').removeClass('sticky');
+	  }	 
+	});
 
 
 
 });
-
-}
-/*
-     FILE ARCHIVED ON 05:20:25 Oct 01, 2011 AND RETRIEVED FROM THE
-     INTERNET ARCHIVE ON 07:12:32 Dec 12, 2023.
-     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
-
-     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
-     SECTION 108(a)(3)).
-*/
-/*
-playback timings (ms):
-  captures_list: 73.666
-  exclusion.robots: 0.15
-  exclusion.robots.policy: 0.134
-  cdx.remote: 0.115
-  esindex: 0.016
-  LoadShardBlock: 38.127 (3)
-  PetaboxLoader3.datanode: 40.048 (4)
-  load_resource: 38.072
-  PetaboxLoader3.resolve: 26.662
-*/
