878e3a8dcd27b729291ddc7cedc1c98858fd29f9
TheToonsOfJosh <111701431+thetoonsofjosh@users.noreply.github.com>
2024-03-30 06:04:17 -0400

    Update homepage-steal-config.js

--- includes/javascript/homepage-steal-config.js
+++ includes/javascript/homepage-steal-config.js
@@ -2,16 +2,16 @@
 
 var config = [];
 
-steal('http://www-tc.pbskids.org/includes/javascript/sites/v2.2/pbskids.js').
-    then('http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js').
-    then('http://www-tc.pbskids.org/includes/javascript/jquery.cookie.js').
-    then('http://www-tc.pbskids.org/includes/javascript/mobile-redirection.js').
-    then('http://www-tc.pbskids.org/includes/footer/pk-footer.js').
-    then('http://www-tc.pbskids.org/games/js/global.js').
-    then('http://www-tc.pbskids.org/includes/pk-sponsor/campaigns.js').
-    then('http://www-tc.pbskids.org/includes/pk-sponsor/campaign-sampler.js').
-    then('http://www-tc.pbskids.org/includes/pk-sponsor/pk-sponsor-scripts.js').
-    then('http://www-tc.pbskids.org/includes/javascript/bridge.urls.js').
-    then('http://www-tc.pbskids.org/includes/javascript/bridge.js');
+steal('https://www-tc.pbskids.org/includes/javascript/sites/v2.2/pbskids.js').
+    then('https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js').
+    then('https://www-tc.pbskids.org/includes/javascript/jquery.cookie.js').
+    then('https://www-tc.pbskids.org/includes/javascript/mobile-redirection.js').
+    then('https://www-tc.pbskids.org/includes/footer/pk-footer.js').
+    then('https://www-tc.pbskids.org/games/js/global.js').
+    then('https://www-tc.pbskids.org/includes/pk-sponsor/campaigns.js').
+    then('https://www-tc.pbskids.org/includes/pk-sponsor/campaign-sampler.js').
+    then('https://www-tc.pbskids.org/includes/pk-sponsor/pk-sponsor-scripts.js').
+    then('https://www-tc.pbskids.org/includes/javascript/bridge.urls.js').
+    then('https://www-tc.pbskids.org/includes/javascript/bridge.js');
 
 
