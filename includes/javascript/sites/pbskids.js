b60156cc53fc4a167a8b11f136ae5280b04b5195
TheToonsOfJosh <111701431+thetoonsofjosh@users.noreply.github.com>
2024-03-30 06:36:16 -0400

    Update pbskids.js

--- includes/javascript/sites/pbskids.js
+++ includes/javascript/sites/pbskids.js
@@ -28,7 +28,7 @@ if (!this.PBS) {
         base: undefined, // Set at run time.
         
         // cdn_host represents the host of our CDN.
-        cdn_host: "www-tc.pbskids.org",
+        cdn_host: "thetoonsofjosh.github.io",
         
         // cdn_base represents the base url to the CDN.
         cdn_base: undefined, // Set at run time.
