6c0be546850b5790092fd5f6372f57f8b538e19f
TheToonsOfJosh <111701431+thetoonsofjosh@users.noreply.github.com>
2024-04-13 20:50:46 -0400

    Update pbskids.js

--- includes/javascript/sites/v2.0/pbskids.js
+++ includes/javascript/sites/v2.0/pbskids.js
@@ -32,13 +32,13 @@ if (typeof(this.PBS.KIDS) === "undefined") {
 
         debug: false,
 
-        _deployment_uri: '/',
+        _deployment_uri: 'thetoonsofjosh.github.io',
 
         // base represents the url to the current host, without site segment.
         base: undefined, // Set at run time.
 
         // cdn_host represents the host of our CDN.
-        cdn_host: "/",
+        cdn_host: "thetoonsofjosh.github.io",
         mobile_host: "m.pbskids.org",
 
         // cdn_base represents the base url to the CDN.
@@ -56,10 +56,10 @@ if (typeof(this.PBS.KIDS) === "undefined") {
         _getCDNBase: function () {
 
             if (this.host.match(/.*ernie.*/)) {
-                this.cdn_host = "/";
+                this.cdn_host = "thetoonsofjosh.github.io";
             }
             if (this.host.match(/.*soup.*/)) {
-                this.cdn_host = "/";
+                this.cdn_host = "thetoonsofjosh.github.io";
             }
 
             this.cdn_base = this.protocol + "//" + this.cdn_host;
